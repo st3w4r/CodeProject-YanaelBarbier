7a8fba924e6d65ba2bc27e05c774ba6f4b7e8022
Yanael Barbier <st3w4r@gmail.com>
2018-12-06 00:09:33 +0100

    Test ChangeApi functions

---
+++ test/api.js
@@ -0,0 +1,33 @@
+const assert = require('assert')
+
+const ChangeApi = require('../src/api')
+
+describe('ChangeApi', () => {
+
+  describe('constructor', () => {
+
+    it('should return a ChangeApi object', () => {
+      const changeApi = new ChangeApi();
+
+      assert.equal(changeApi.host, 'https://www.change.org');
+    })
+
+    it('should sign a petition', async () => {
+
+      const changeApi = new ChangeApi();
+
+      const petitionId = 13954429;
+      const firstName = 'Yana';
+      const lastName = 'Bar';
+      const email = 'yana@bar.com';
+
+
+      try {
+      const res = await changeApi.signPetition(petitionId, firstName, lastName, email);
+        console.log(res);
+      } catch (e) {
+        console.error(e);
+      }
+    })
+  })
+})
