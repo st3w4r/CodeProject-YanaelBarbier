b879867d06a59b19b82ad72714f903a55acbd9af
Yanael Barbier <st3w4r@gmail.com>
2018-12-14 23:12:10 +0100

    One by one nightmare process

--- src/index.js
+++ src/index.js
@@ -5,21 +5,27 @@ const usersList = new UsersList('csv', './data/users.csv');
 
 const start = async () => {
   const users = await usersList.getUsers();
+  const scrapper = new Scrapper();
+
+  let p = Promise.resolve();
 
   users.forEach(async (user) => {
-    try {
-      const scrapper = new Scrapper();
-      const response = await scrapper.signPetition(
-        user.first_name,
-        user.last_name,
-        user.email)
-      if (response) {
-        console.log(response);
+    p = p.then(async () => {
+      try {
+        const response = await scrapper.signPetition(
+          user.first_name,
+          user.last_name,
+          user.email)
+        if (response) {
+          console.log(response);
+          console.log(user);
+        }
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e);
-    }
+    });
   });
+  return p;
 };
 
 module.exports = start;
--- src/scrapper.js
+++ src/scrapper.js
@@ -9,7 +9,9 @@ class Scrapper {
   signPetition(firstName, lastName, email) {
     return this.nightmare
       .viewport(1920, 1080)
+      .cookies.clearAll()
       .goto('https://www.change.org/p/yana%C3%ABl-barbier-hire-yana%C3%ABl-barbier')
+      .refresh()
       .click('#first_name')
       .type('#first_name', firstName)
       .click('#last_name')
@@ -18,18 +20,20 @@ class Scrapper {
       .type('#content > div > div:nth-child(4) > div.row.mbxxl.xs-mbn.xs-mhn > div.js-petition-action-panel-container.col-xs-12.col-sm-4.xs-phn > div > div > div > div > div.js-sign-and-share-components > div > div.mts > div > div > form > div.grouped-form.mvxs > div.control-group > div > span > input', email)
       .check('#marketing_comms_consent_false')
       .check('#not_public')
-      .screenshot(`/app/screenshots/change_form_${firstName}.png`)
+      .screenshot(`/app/screenshots/change_form_${email}.png`)
       .click('#content > div > div:nth-child(4) > div.row.mbxxl.xs-mbn.xs-mhn > div.js-petition-action-panel-container.col-xs-12.col-sm-4.xs-phn > div > div > div > div > div.js-sign-and-share-components > div > div.mts > div > div > div:nth-child(2) > div > button')
-      .screenshot(`/app/screenshots/change_submitted_${firstName}.png`)
-      .end();
+      .screenshot(`/app/screenshots/change_submitted_${email}.png`)
   }
 
-  checkEmail(url, firstName) {
+  checkEmail(url, email) {
     return this.nightmare
       .viewport(1920, 1080)
       .goto(url)
-      .screenshot(`/app/screenshots/change_check_${firstName}.png`)
-      .end();
+      .screenshot(`/app/screenshots/change_check_${email}.png`)
+  }
+
+  end() {
+    return this.nightmare.end();
   }
 }
 
